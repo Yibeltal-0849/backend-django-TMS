e3ac69af9cd3c54587cfa795b0af9279dbdc0323
yibeltal <yibeltal2514@gmail.com>
2025-12-23 23:05:10 +0300

    JWT AUTHENTICATION

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -75,3 +75,27 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,12 +1,31 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/projects/', include('apps.projects.urls')),
+#     path('api/tasks/', include('apps.tasks.urls')),
+
+#     # Root path
+#     path('', lambda request: HttpResponse("Welcome to Task Manager API")),
+# ]
+
+
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/projects/', include('apps.projects.urls')),
-    path('api/tasks/', include('apps.tasks.urls')),
 
-    # Root path
-    path('', lambda request: HttpResponse("Welcome to Task Manager API")),
+    # JWT
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/', include('apps.projects.urls')),
+    path('api/', include('apps.tasks.urls')),
 ]
