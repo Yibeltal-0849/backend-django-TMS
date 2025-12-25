2aadea7b0911a02a98c4ca63fa0395c6aeb0d142
yibeltal <yibeltal2514@gmail.com>
2025-12-25 19:31:28 +0300

    fixed

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,7 +1,24 @@
+# from rest_framework.viewsets import ModelViewSet
+# from .models import Project
+# from .serializers import ProjectSerializer
+
+# class ProjectViewSet(ModelViewSet):
+#     queryset = Project.objects.all()
+#     serializer_class = ProjectSerializer
+
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from .models import Project
 from .serializers import ProjectSerializer
+from .permissions import IsOwner
 
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Project.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,31 +1,22 @@
-# from django.http import HttpResponse
-# from django.contrib import admin
-# from django.urls import path, include
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/projects/', include('apps.projects.urls')),
-#     path('api/tasks/', include('apps.tasks.urls')),
-
-#     # Root path
-#     path('', lambda request: HttpResponse("Welcome to Task Manager API")),
-# ]
-
-
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
+    # Root path - THIS IS WHAT'S MISSING
+    path('', lambda request: HttpResponse("Welcome to Task Manager API")),
+    
     path('admin/', admin.site.urls),
 
     # JWT
     path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+    # Don't forget to include your projects and tasks URLs too!
     path('api/', include('apps.projects.urls')),
     path('api/', include('apps.tasks.urls')),
 ]
